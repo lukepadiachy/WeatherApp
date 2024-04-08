501075e2c5b796755bfd18211b23f3df38138d7a
Padi <148848073+lukepadiachy@users.noreply.github.com>
2024-04-08 20:20:27 +0200

    woohoo

--- WeatherApp/Pages/WeatherInfoPage.xaml.cs
+++ WeatherApp/Pages/WeatherInfoPage.xaml.cs
@@ -5,10 +5,42 @@ namespace WeatherApp.Pages;
 
 public partial class WeatherInfoPage : ContentPage
 {
-    private HttpClient _client;
+    
     public WeatherInfoPage()
 	{
 		InitializeComponent();
         
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        BindingContext = await GetWeatherData();
+    }
+
+    private async Task<OpenWeatherData> GetWeatherData() 
+    { 
+        var details =await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+        if (details != PermissionStatus.Granted)
+        {
+            details = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+        }
+        var location = await Geolocation.GetLocationAsync();
+        
+        var lat = location.Latitude;
+        var lon = location.Longitude;
+
+        var client = new HttpClient();
+
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+        string url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid=13f3c4e6aaae5782ec1fd60e26c43d65";
+
+        var response = await client.GetStringAsync(url);
+
+        var weatherData = JsonConvert.DeserializeObject<OpenWeatherData>(response);
+
+        return weatherData;
+
     }
 } 
